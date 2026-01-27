8e939188a99c05105c49a0e815a66552bf360cc3
crarm <61692711+crarm@users.noreply.github.com>
2026-01-27 22:45:47 +0800

    Update argosbx.sh

--- argosbx.sh
+++ argosbx.sh
@@ -1,3 +1,4 @@
+echo yyyyy
   sleep 300
   if find /proc/*/exe -type l 2>/dev/null | grep -E '/proc/[0-9]+/exe' | xargs -r readlink 2>/dev/null | grep -Eq 'agsbx/(s|x)' || pgrep -f 'agsbx/(s|x)' >/dev/null 2>&1; then
     echo "Argosbx脚本进程启动成功，安装完毕" && sleep 2
