6cb3fc93de3e85e0a4ea2776ca1e399691a99a39
crarm <61692711+crarm@users.noreply.github.com>
2026-01-27 12:30:28 +0800

    Update ttyd.sh

--- google-idx/ttyd.sh
+++ google-idx/ttyd.sh
@@ -3,9 +3,11 @@
 USER="${USER:-google}"
 PASS="${PASS:-123456}"
 
-mkdir ttyd
-cd ttyd
+mkdir app/ttyd
+cd app/ttyd
 wget -O ttyd https://github.com/tsl0922/ttyd/releases/latest/download/ttyd.x86_64
-chmod +x ttyd
-nohup $PWD/ttyd -p 7681 -c $USER:$PASS -W bash 1>/dev/null 2>&1 &
+
+echo nohup $PWD/ttyd -p 7681 -c $USER:$PASS -W bash 1>/dev/null 2>&1 & > startup.sh
+chmod +x ttyd startup.sh
+./startup.sh
 echo "当前用户名为：$USER，密码为：$PASS"
