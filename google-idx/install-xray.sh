740c26b6642421dfc02c4107603008a918765cef
crarm <61692711+crarm@users.noreply.github.com>
2026-01-27 11:06:28 +0800

    Update install-xray.sh

--- google-idx/install-xray.sh
+++ google-idx/install-xray.sh
@@ -3,6 +3,27 @@
 PORT="${PORT:-8080}"
 UUID="${UUID:-2584b733-9095-4bec-a7d5-62b473540f7a}"
 
+readp "设置节点使用的端口[1-65535]（回车跳过为10000-65535之间的随机端口）：" port
+sleep 1
+if [[ -z $PORT ]]; then
+PORT=$(shuf -i 10000-65535 -n 1)
+until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") ]] 
+do
+[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") ]] && yellow "\n端口被占用，请重新输入端口" && readp "自定义端口:" PORT
+done
+else
+until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") ]]
+do
+[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") ]] && yellow "\n端口被占用，请重新输入端口" && readp "自定义端口:" PORT
+done
+fi
+sleep 1
+
+read -p "请输入UUID（默认为3ef440dc-8eac-4d33-b50d-382f54507e0c）：" UUID
+if [[ -z ${UUID} ]]; then
+UUID='3ef440dc-8eac-4d33-b50d-382f54507e0c'
+fi
+
 # 1. init directory
 Xray=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mkdir -p app/$Xray
@@ -32,6 +53,7 @@ $PWD/startup.sh
 read -p "请输入$PORT端口的外部访问链接：" DOMAIN
 
 # 7. print node info
+echo '节点信息如下：'
 echo '---------------------------------------------------------------'
 echo "vless://$UUID@e$DOMAIN:443?encryption=none&security=tls&alpn=http%2F1.1&fp=chrome&type=xhttp&path=%2F&mode=auto#idx-xhttp"
 echo '---------------------------------------------------------------'
