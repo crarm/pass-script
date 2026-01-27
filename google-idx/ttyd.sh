aee6b1dff4d7a2586eaa4c31881f24b2fb8f75b4
crarm <61692711+crarm@users.noreply.github.com>
2026-01-27 12:35:08 +0800

    Update ttyd.sh

--- google-idx/ttyd.sh
+++ google-idx/ttyd.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env sh
 
-USER="${USER:-google}"
-PASS="${PASS:-123456}"
+RUSER="google"
+RPASS="123456"
+RPORT="2458"
+
+read -p "请输入用户名（直接回车使用默认值：$RUSER）：" USER
+if [[ -z ${USER} ]]; then
+    USER=$RUSER
+fi
+
+read -p "请输入密码（直接回车使用默认值：$RPASS）：" PASS
+if [[ -z ${PASS} ]]; then
+    PASS=$RPASS
+fi
+
+read -p "请输入端口（直接回车使用默认值：$RPORT）：" PASS
+if [[ -z ${PORT} ]]; then
+    PORT=$RPORT
+fi
 
 mkdir app/ttyd
 cd app/ttyd
 wget -O ttyd https://github.com/tsl0922/ttyd/releases/latest/download/ttyd.x86_64
 
-echo nohup $PWD/ttyd -p 7681 -c $USER:$PASS -W bash 1>/dev/null 2>&1 & > startup.sh
+echo nohup $PWD/ttyd -p $PORT -c $USER:$PASS -W bash 1>/dev/null 2>&1 & > startup.sh
 chmod +x ttyd startup.sh
 ./startup.sh
 echo "当前用户名为：$USER，密码为：$PASS"
