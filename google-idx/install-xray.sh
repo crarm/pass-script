a41e78703d64ceccb8b03bbdb9eb8bb75e6f803a
crarm <61692711+crarm@users.noreply.github.com>
2026-01-27 09:23:05 +0800

    Create install-xray.sh

---
+++ google-idx/install-xray.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+PORT="${PORT:-8080}"
+UUID="${UUID:-2584b733-9095-4bec-a7d5-62b473540f7a}"
+
+# 1. init directory
+Xray=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
+mkdir -p app/$Xray
+cd app/$Xray
+
+# 2. download and extract Xray
+wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -O $Xray.zip
+unzip $Xray.zip
+mv xray $Xray
+rm -f $Xray.zip
+
+# 3. add config file
+wget -O config.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/google-idx/xray-config-template.json
+sed -i 's/$PORT/'$PORT'/g' config.json
+sed -i 's/$UUID/'$UUID'/g' config.json
+
+# 4. create startup.sh
+wget https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/google-idx/startup.sh
+sed -i 's#$PWD#'$PWD'#g' startup.sh
+chmod +x startup.sh
+
+# 5. start Xray
+$PWD/startup.sh
+
+# 6. print node info
+echo '---------------------------------------------------------------'
+echo "vless://$UUID@example.domain.com:443?encryption=none&security=tls&alpn=http%2F1.1&fp=chrome&type=xhttp&path=%2F&mode=auto#idx-xhttp"
+echo '---------------------------------------------------------------'
