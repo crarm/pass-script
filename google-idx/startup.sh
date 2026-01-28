07401bc9a13994e728f45cd2070d1a864ab65514
crarm <61692711+crarm@users.noreply.github.com>
2026-01-28 16:56:31 +0800

    Fix startup script to include cloudflared command

--- google-idx/startup.sh
+++ google-idx/startup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
 nohup $PWD/$Xray -c $PWD/config.json 1>$PWD/$Xray.log 2>&1 &
+nohup $PWD/cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 run --token "$ARGO_AUTH" >/dev/null 2>&1 &
