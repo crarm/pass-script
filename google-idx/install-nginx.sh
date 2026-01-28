aeabbe69d36c5803f29553ce764846ea33b1048e
crarm <61692711+crarm@users.noreply.github.com>
2026-01-28 17:16:31 +0800

    Delete google-idx/install-nginx.sh

--- google-idx/install-nginx.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p /var/log/nginx
-touch /var/log/nginx/error.log
-touch /var/log/nginx/access.log
-chmod -R 755 /var/log/nginx
-
-# 创建 PID 文件目录
-mkdir -p /var/run/nginx
-touch /var/run/nginx/nginx.pid
-chmod 644 /var/run/nginx/nginx.pid
