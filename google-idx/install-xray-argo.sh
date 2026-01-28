f7fe4d6c8965ca390b79d3ba9dea6680de55145c
crarm <61692711+crarm@users.noreply.github.com>
2026-01-28 16:55:29 +0800

    Update install-xray-argo.sh

--- google-idx/install-xray-argo.sh
+++ google-idx/install-xray-argo.sh
@@ -1,17 +1,49 @@
 #!/usr/bin/env sh
 
-read -p "设置节点使用的端口[1-65535]（回车跳过为10000-65535之间的随机端口）：" PORT
+read -p "设置vmess-ws节点使用的端口[1-65535]（回车跳过为10000-65535之间的随机端口）：" VMWSPORT
 sleep 1
-if [[ -z $PORT ]]; then
-PORT=$(shuf -i 10000-65535 -n 1)
-until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") ]] 
+if [[ -z $VMWSPORT ]]; then
+VMWSPORT=$(shuf -i 10000-65535 -n 1)
+until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VMWSPORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VMWSPORT") ]] 
 do
-[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") ]] && yellow "\n端口被占用，请重新输入端口" && read -p "自定义端口:" PORT
+[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VMWSPORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VMWSPORT") ]] && yellow "\n端口被占用，请重新输入端口" && read -p "自定义端口:" PORT
 done
 else
-until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") ]]
+until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VMWSPORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VMWSPORT") ]]
 do
-[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$PORT") ]] && yellow "\n端口被占用，请重新输入端口" && read -p "自定义端口:" PORT
+[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VMWSPORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VMWSPORT") ]] && yellow "\n端口被占用，请重新输入端口" && read -p "自定义端口:" PORT
+done
+fi
+sleep 1
+
+read -p "设置vless-ws节点使用的端口[1-65535]（回车跳过为10000-65535之间的随机端口）：" VLWSPORT
+sleep 1
+if [[ -z $VLWSPORT ]]; then
+VLWSPORT=$(shuf -i 10000-65535 -n 1)
+until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLWSPORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLWSPORT") ]] 
+do
+[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLWSPORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLWSPORT") ]] && yellow "\n端口被占用，请重新输入端口" && read -p "自定义端口:" PORT
+done
+else
+until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLWSPORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLWSPORT") ]]
+do
+[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLWSPORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLWSPORT") ]] && yellow "\n端口被占用，请重新输入端口" && read -p "自定义端口:" PORT
+done
+fi
+sleep 1
+
+read -p "设置vless-xhttp节点使用的端口[1-65535]（回车跳过为10000-65535之间的随机端口）：" VLXHTTPPORT
+sleep 1
+if [[ -z $VLXHTTPPORT ]]; then
+VLXHTTPPORT=$(shuf -i 10000-65535 -n 1)
+until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLXHTTPPORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLXHTTPPORT") ]] 
+do
+[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLXHTTPPORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLXHTTPPORT") ]] && yellow "\n端口被占用，请重新输入端口" && read -p "自定义端口:" PORT
+done
+else
+until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLXHTTPPORT") && -z $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLXHTTPPORT") ]]
+do
+[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLXHTTPPORT") || -n $(ss -tunlp | grep -w tcp | awk '{print $5}' | sed 's/.*://g' | grep -w "$VLXHTTPPORT") ]] && yellow "\n端口被占用，请重新输入端口" && read -p "自定义端口:" PORT
 done
 fi
 sleep 1
@@ -33,9 +65,12 @@ mv xray $Xray
 rm -f $Xray.zip
 
 # 3. add config file
-wget -O config.json https://raw.githubusercontent.com/crarm/pass-script/refs/heads/main/google-idx/xray-config-template.json
-sed -i 's/$PORT/'$PORT'/g' config.json
+wget -O config.json https://raw.githubusercontent.com/crarm/pass-script/refs/heads/main/google-idx/xray-config.json
 sed -i 's/$UUID/'$UUID'/g' config.json
+sed -i 's#$VMWSPORT#'$VMWSPORT'#g' config.json
+sed -i 's#$VLWSPORT#'$VLWSPORT'#g' config.json
+sed -i 's#$VLXHTTPPORT#'$VLXHTTPPORT'#g' config.json
+sed -i 's#$UUID#'$UUID'#g' config.json
 
 # 4. create startup.sh
 wget https://raw.githubusercontent.com/crarm/pass-script/refs/heads/main/google-idx/startup.sh
@@ -47,7 +82,14 @@ chmod +x startup.sh
 $PWD/startup.sh
 
 # 6. enter export domain
-read -p "请输入端口【 $PORT 】的外部访问链接：" DOMAIN
+echo "！请打开vless-xhttp端口【 $VLXHTTPPORT 】的外部访问！"
+read -p "请输入vless-xhttp端口【 $VLXHTTPPORT 】的外部访问链接：" DOMAIN
+
+# 7. download cloudflared
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cloudflared
+chmod +x cloudflared
+read -p "请输入cloudflare TOKEN【 以eyJh开头 】：" ARGO_AUTH
+nohup "cloudflared" tunnel --no-autoupdate --edge-ip-version auto --protocol http2 run --token "${ARGO_AUTH}" >/dev/null 2>&1 &
 
 # 7. print node info
 echo '节点信息如下：'
@@ -58,10 +100,7 @@ echo "节点信息保存在文件：$PWD/node-info.txt"
 echo "vless://$UUID@$DOMAIN:443?encryption=none&security=tls&alpn=http%2F1.1&fp=chrome&type=xhttp&path=%2F&mode=auto#idx-xhttp" > node-info.txt
 
 
-sed -i 's#$VMWSPORT#'$VMWSPORT'#g' config.json
-sed -i 's#$VLWSPORT#'$VLWSPORT'#g' config.json
-sed -i 's#$VLXHTTPPORT#'$VLXHTTPPORT'#g' config.json
-sed -i 's#$UUID#'$UUID'#g' config.json
+
 
 
 echo vmess-ws argo:> node.info
